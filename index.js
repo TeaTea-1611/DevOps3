e0691f779874777ad3d0af910b7870be98595485
TeaTea-1611 <phamnnam079202038134@gmail.com>
2023-12-05 22:22:11 +0700

    test

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
 
+app.get("/users", function (req, res) {
+  res.json({
+    data: [
+      {
+        id: 1,
+        name: "user01"
+      },
+      {
+        id: 2,
+        name: "user02"
+      }
+    ]
+  })
+});
+
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
